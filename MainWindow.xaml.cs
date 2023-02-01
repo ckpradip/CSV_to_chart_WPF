3d61d32b66186c9ea1888710e80cc07d8cdbcaf2
Pradeep <ckpradip@gmail.com>
2023-02-01 17:29:17 +1100

    Fixed the following: 1. scaling factor 2. select/unselect chart items

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace CSV_to_chart_WPF
     }
 
 
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -31,17 +32,14 @@ namespace CSV_to_chart_WPF
 
         ArrayList line_color = new ArrayList();
 
-
+        double[][] arr;
+        int number_of_fields = 0;
+        double[] counter;
+        ArrayList al;
 
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-
-        private void init_chart_plot()
-        {
-
         }
 
 
@@ -64,8 +62,6 @@ namespace CSV_to_chart_WPF
         {
             using (TextFieldParser csv_parser = new TextFieldParser(textbox_filename.Text))
             {
-                int number_of_fields = 0;
-
                 csv_parser.TextFieldType = FieldType.Delimited;
                 csv_parser.SetDelimiters(",");
 
@@ -91,7 +87,7 @@ namespace CSV_to_chart_WPF
                 }
 
                 /* Read remaining values from csv file */
-                ArrayList al = new ArrayList();
+                al = new ArrayList();
                 while (!csv_parser.EndOfData)
                 {
                     string[] fields = csv_parser.ReadFields();
@@ -99,40 +95,72 @@ namespace CSV_to_chart_WPF
                 }
 
                 /* Create dynamic 2-dimensional array */
-                double[][] arr = new double[number_of_fields][];
-                double[] counter = new double[al.Count];
+                arr = new double[number_of_fields][];
+                counter = new double[al.Count];
                 for (int field = 0; field < number_of_fields; field++)
                 {
                     arr[field] = new double[al.Count];
                 }
                 
-                /* add entries into chart array */
-                for (int count = 0; count < al.Count; count++)
-                {
-                    string[] fields = (string[])al[count];
-                    counter[count] = count;
+                load_values();
 
-                    for (int field = 0; field < number_of_fields; field++)
-                    {
-                        arr[field][count] = Convert.ToDouble(fields[field]);
-                    }
-                }
+                load_chart();
+            }
+        }
 
-                /* add value into chart area and customize the chart properties */
+
+        private void load_values()
+        {
+            /* add entries into chart array */
+            for (int count = 0; count < al.Count; count++)
+            {
+                string[] fields = (string[])al[count];
+                counter[count] = count;
+
                 for (int idx = 0; idx < number_of_fields; idx++)
                 {
                     chart_header item = (chart_header)listbox_display_header.ItemContainerGenerator.Items[idx];
-                    
+                    double scaling_factor = Convert.ToDouble(item.chart_scale_factor);
+                    if ((scaling_factor < 0)) scaling_factor = 1.0;
+
+                    arr[idx][count] = Convert.ToDouble(fields[idx]) * scaling_factor;
+                }
+            }
+        }
+
+
+        private void load_chart()
+        {
+            scottplot_chart.Plot.Clear();
+
+            /* add value into chart area and customize the chart properties */
+            for (int idx = 0; idx < number_of_fields; idx++)
+            {
+                chart_header item = (chart_header)listbox_display_header.ItemContainerGenerator.Items[idx];
+
+                if (item.chart_ischecked == true)
+                {
                     var signal = scottplot_chart.Plot.AddScatterLines(counter, arr[idx]);
                     var temp_color = (Color)line_color[idx];
                     signal.LineColor = System.Drawing.Color.FromArgb(temp_color.A, temp_color.R, temp_color.G, temp_color.B);
                     signal.Label = item.chart_Title;
-
-                    var new_axes = scottplot_chart.Plot;
                 }
 
-                scottplot_chart.Refresh();
+                //var new_axes = scottplot_chart.Plot;
             }
+
+            scottplot_chart.Refresh();
+        }
+
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            load_chart();
+        }
+
+        private void textbox_scale_factor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            load_values();
+            load_chart();
         }
     }
 }
